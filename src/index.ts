e24bf85ea7ab896282dfbaf09522ac982ee014eb
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-02-07 14:57:16 +0200

    project init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import resolvers from './resolvers';
+import typeDefs from './schema';
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`🚀  Server ready at: ${url}`);
+});
