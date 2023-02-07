4d93dbf9a3e38d85a9c74940c339207fb37aa7f3
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-02-07 16:27:08 +0200

    service architecture

--- src/resolvers/book-resolver.ts
+++ src/resolvers/book-resolver.ts
@@ -1,14 +1,6 @@
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
+import { getBooks } from '../services/book-service';
 
-export function getBooks() {
+export function books() {
+  const books = getBooks();
   return books;
 }
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,10 @@
-import { getBooks } from './book-resolver';
+import { books } from './book-resolver';
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: getBooks,
+    books,
   },
 };
 
