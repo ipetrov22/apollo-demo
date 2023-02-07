2df0a658480d4366ad96484360e05a7e8fc9669a
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-02-07 15:38:36 +0200

    resolver architecture

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,19 +1,10 @@
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
+import { getBooks } from './book-resolver';
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: getBooks,
   },
 };
 
