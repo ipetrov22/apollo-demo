b85b6a00e315393afe3a55b5793dc26cea53710c
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-02-08 15:51:01 +0200

    add pair/token schema types

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -10,6 +10,49 @@ const typeDefs = `#graphql
     author: String
   }
 
+  # The "BigGQL" scalar type to represent currency values
+  scalar BigGQL
+
+  type Pair {
+    id: String!
+    pairAddress: String
+    token0: Token!
+    token1: Token!
+    reserve0: BigGQL!
+    reserve1: BigGQL!
+    totalSupply: BigGQL!
+    reserveHYDRA: BigGQL!
+    reserveUSD: BigGQL!
+    trackedReserveHYDRA: BigGQL!
+    token0Price: BigGQL!
+    token1Price: BigGQL!
+    volumeToken0: BigGQL!
+    volumeToken1: BigGQL!
+    volumeUSD: BigGQL!
+    untrackedVolumeUSD: BigGQL!
+    txCount: Int!
+    createdAtTimestamp: Int!
+    createdAtBlockNumber: Int!
+    liquidityProviderCount: Int!
+    _oneDayVolumeUSD: BigGQL!
+    _oneWeekVolumeUSD: BigGQL!
+  }
+
+  type Token {
+    id: String!
+    tokenAddress: String!
+    symbol: String!
+    name: String!
+    decimals: Int!
+    totalSupply: Int!
+    tradeVolume: BigGQL!
+    tradeVolumeUSD: BigGQL!
+    untrackedVolumeUSD: BigGQL!
+    txCount: Int!
+    totalLiquidity: BigGQL!
+    derivedHYDRA: BigGQL!
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
