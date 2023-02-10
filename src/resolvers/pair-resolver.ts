79a54d813f019ad03e39d31b24f1cdf9b1d4c151
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-02-10 14:00:51 +0200

    get v2 pairs service

--- src/resolvers/pair-resolver.ts
+++ src/resolvers/pair-resolver.ts
@@ -1,13 +1,10 @@
-import request from 'graphql-request';
-
-import { pairsQuery } from '../queries/pair';
 import { PairsArgs } from '../schema/pair';
-import { getEnvVariable } from '../utils/getEnvVariable';
+import { getV2Pairs } from '../services/pair-service';
 
-export function pairs(_, args: PairsArgs) {
-  const url = getEnvVariable('V2_GRAPH_ENDPOINT');
-  request(url, pairsQuery).then(console.log).catch(console.log);
-  const pairs = [];
+export async function pairs(_, args: PairsArgs) {
+  const v2Pairs = await getV2Pairs(args);
+
+  const pairs = [...v2Pairs];
   return pairs;
 }
 
--- src/services/pair-service.ts
+++ src/services/pair-service.ts
@@ -1,7 +1,12 @@
+import request from 'graphql-request';
+import { pairsQuery } from '../queries/pair';
 import { PairsArgs } from '../schema/pair';
+import { getEnvVariable } from '../utils/getEnvVariable';
 
-export function getV2Pairs(args: PairsArgs) {
-  return [];
+export async function getV2Pairs(args: PairsArgs) {
+  const url = getEnvVariable('V2_GRAPH_ENDPOINT');
+  const data = await request(url, pairsQuery, args);
+  return data.pairs ? data.pairs : [];
 }
 
 export function getV3Pools() {
